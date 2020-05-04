ff6ab464cf67ae590580defe1dfe0c6a2ba50c59
SamDbs <dbs.samantha@gmail.com>
2020-05-04 15:46:01 +0200

    Ajout templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,14 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    public function indexAction(){
+    public function indexAction()
+    {
         $userName = 'Julien';
         return $this->render('home.html.twig', ["name" => $userName]);
     }
 
-    public function aboutAction(){
-
-        
+    public function aboutAction()
+    {
+        $fruits = [
+            ["name" => "berry", "price" => 8, "allergy" => ['pollen', 'autre chose']],
+            ["name" => "coconut", "price" => 12]
+        ];
+        return $this->render('about.html.twig', ["fruits" => $fruits]);
     }
 
+
+    public function nameAction($name)
+    {
+        return $this->render('name.html.twig', ["name" => $name]);
+    }
 }
